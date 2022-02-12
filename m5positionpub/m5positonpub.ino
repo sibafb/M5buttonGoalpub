e7e95f3ab631fd9a15fe660137bf8a362af1c6b4
AkimotoHaruma <siba16fb@gmail.com>
2022-02-12 15:17:03 +0900

    first commit

---
+++ m5positionpub/m5positonpub.ino
@@ -0,0 +1,32 @@
+#include <M5Stack.h>
+#include <ros.h>
+#include <std_msgs/Bool.h>
+
+const int BUTTUN_PIN = 30;
+
+std_msgs::Bool buttton_pushed_msg;
+ros::Publisher pub_bottun_pushed("buttun_pushed", &buttton_pushed_msg);
+ros::NodeHandle nh;
+
+void setup() {
+  pinMode(BUTTUN_PIN, INPUT);
+
+  nh.initNode();
+  nh.advertise(pub_bottun_pushed);
+
+  nh.loginfo("push button ready");
+}
+
+void loop() {
+
+  if( digitalRead(BUTTUN_PIN) == HIGH ){
+    buttton_pushed_msg.data = true;
+  }
+  else{
+    buttton_pushed_msg.data = false;
+  }
+
+  pub_bottun_pushed.publish(&buttton_pushed_msg);
+  nh.spinOnce();
+  delay(30); //ms
+}
